ab236dcf6a3264441df56f4c2dcb7c3464fbf703
Pyropingouin <88858653+Pyropingouin@users.noreply.github.com>
2025-11-15 14:39:44 -0500

    comments

--- HockeyPrototype/scene/ice_map_layer.gd
+++ HockeyPrototype/scene/ice_map_layer.gd
@@ -3,9 +3,11 @@ extends TileMapLayer
 var cell_info: Dictionary = {}
 
 
-# Called when the node enters the scene tree for the first time.
+
 func _ready() -> void:
+	#Retourne une liste de toute les cellules ou j'ai peint une tuile. 
 	for cell in get_used_cells():
+		#Assigne des propriété à ces tuile dans le dictionnaire
 		cell_info[cell] = {
 			"type" :"ice",
 			"cost": 1,
