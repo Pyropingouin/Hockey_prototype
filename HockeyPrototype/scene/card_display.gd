ad82cbb728ca11a3a3f2dc06a3b44a1f4eabf8a2
Pyropingouin <88858653+Pyropingouin@users.noreply.github.com>
2025-12-16 18:47:45 -0500

    add Ring to Pawn + display Stats on card

--- HockeyPrototype/scene/card_display.gd
+++ HockeyPrototype/scene/card_display.gd
@@ -11,4 +11,7 @@ func _ready():
 func _on_ice_map_layer_pawn_selected(pawn: Variant) -> void:
 	print("Pawn Recu", pawn)
 	sprite.texture = pawn.fullBodyTexture
-	
+	$Move.text = "SPEED: " + str(pawn.move_range)
+
+	$Strength.text = "STRENGTH: " +str(pawn.strength)
+	$Reflex.text = "REFLEX: " +str(pawn.reflex)
--- HockeyPrototype/scene/pawn.gd
+++ HockeyPrototype/scene/pawn.gd
@@ -3,6 +3,8 @@ extends CharacterBody2D
 @export var bubbleHeadTexture: Texture2D
 @export var fullBodyTexture: Texture2D
 @export var move_range: int = 2
+@export var strength: int = 2
+@export var reflex: int = 3
 @export var start_cell: Vector2i = Vector2i(0, 0):
 	set(value):
 		start_cell = value
@@ -14,12 +16,26 @@ var current_cell = start_cell
 
 @onready var sprite: Sprite2D = $Sprite2D
 
+#DEBUG FOR TEAMS
+@onready var ring: Sprite2D = $Ring
+
 
 
 func _ready():
 
 	if bubbleHeadTexture:
 		sprite.texture = bubbleHeadTexture
+		
+	#DEBUG FOR TEAMS
+	if team_id == 1:
+		ring.modulate = Color.RED
+	#DEBUG FOR TEAMS
+	else:
+		ring.modulate = Color.BLUE	
+		
 
 func get_current_cell() -> Vector2i:
 	return current_cell
+	
+	
+	
